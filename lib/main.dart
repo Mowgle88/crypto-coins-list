8ba46bc5b8b0e8471a11e586a73c7c0a2ab12fe8
Dmitry Ilyin <mowgle>
2023-04-27 02:20:18 +0300

    feat: add AbstractCoinsRepository

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'crypto_coins_list_app.dart';
 
 void main() {
+  GetIt.I.registerSingleton<AbstractCoinsRepository>(
+      CryptoCoinRepository(dio: Dio()));
   runApp(const CryptoCurrenciesListApp());
 }
